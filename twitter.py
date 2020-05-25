45547a5fd782e3dbcdd2a5965791fe79141fa2c0
Rrezart <rb37811@universum-ks.org>
2020-05-25 08:17:04 +0200

    veq edhe me doker me ba ka met

--- twitter.py
+++ twitter.py
@@ -1,34 +1,47 @@
 from twitterscraper import query_tweets_from_user
-import datetime as dt
+from time import sleep
+from colorama import Fore, Back, Style 
 import pyodbc 
 
 #lang = "english"
 
 #Connection with DD
-conn = pyodbc.connect('Driver={SQL Server};''Server=DESKTOP-585PVI8;''Database=Forliza;''Trusted_Connection=yes;')
+conn = pyodbc.connect('Driver={SQL Server};''Server=DESKTOP-585PVI8;''Database=Forliza2;''Trusted_Connection=yes;')
 cursor = conn.cursor()
 
-#Retriving analyst usernames from DB
-analysts = []
-analystsSelect = cursor.execute('select Username from Analysts')
-for analyst in analystsSelect:
-    analysts.append(analyst.Username)
 
-#Adding tweets in DB...
-for analyst in analysts:
-    list_of_tweets =  query_tweets_from_user(analyst, limit = 1)
-    for tweet in list_of_tweets:
-        originalTweet = False
-        #...only the original tweets
-        for analyst in analysts:
-            if tweet.screen_name == analyst:
-                originalTweet = True
-                break
-        if originalTweet:
-            params = (tweet.username,tweet.screen_name,tweet.text)
-            conn.execute("{CALL InsertTweets (?,?,?)}", params)
+while True:
+    #Retriving analyst usernames and Guids from DB
+    analysts = []
+    analystsID = []
+    analystsSelect = cursor.execute('select Username,Id from Analysts').fetchall()
+    for analyst in analystsSelect:
+        analysts.append(analyst.Username)
+        analystsID.append(analyst.Id)
+
+    #Adding tweets in DB...
+    for (analyst,analystID) in zip(analysts,analystsID):
+        list_of_tweets =  query_tweets_from_user(analyst, limit = 1)
+        if list_of_tweets == []:
+            print(Fore.RED+"Private user or no post")
+            print(Fore.RESET)
+            continue
+        last_tweet = list_of_tweets[0]
+        try:
+            inserted_last = cursor.execute("select top 1 Tweet from AnalystTweets where AnalystID = '"+analystID+"' order by Id desc").fetchone()
+        except TypeError:
+            inserted_last = ('',)
+            
+        if inserted_last != None:
+            inserted_last = inserted_last.Tweet
+
+        if last_tweet.screen_name == analyst and last_tweet.text != inserted_last:
+            params = (analystID,last_tweet.text)
+            conn.execute("{CALL InsertTweets (?,?)}", params)
             conn.commit()
-
-
-        
-
+    sleeptime = 10
+    sleep(sleeptime)
+    # for i in range(sleeptime):
+    #     print(i+1)
+    #     sleep(1)
+            
